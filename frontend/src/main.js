8aae2f4fd88d5674f58a26b45822032e44e0772a
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2023-10-23 16:58:43 -0600

    Update main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router.js';
+import BaseButton from '../src/components/ui/BaseButton.vue';
 
 
 
 const app = createApp(App);
 
 app.use(router);
+app.component('base-button', BaseButton);
 
 app.mount('#app')
