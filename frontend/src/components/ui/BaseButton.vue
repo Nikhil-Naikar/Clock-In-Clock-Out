c5cce9dc2c1072d078e67033a031806148b65bd7
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2023-10-23 16:58:41 -0600

    Create BaseButton.vue

---
+++ frontend/src/components/ui/BaseButton.vue
@@ -0,0 +1,26 @@
+<template>
+    <button>
+        <slot>
+        </slot>
+    </button>
+</template>
+
+<style scoped>
+button{
+  text-decoration: none;
+  padding: 0.75rem 1.5rem;
+  font: inherit;
+  background-color: rgb(3, 107, 128);
+  border: 1px solid rgb(98, 189, 207);
+  color: white;
+  cursor: pointer;
+  display: inline-block;
+}
+
+button:hover,
+button:active {
+  background-color: rgb(98, 189, 207);
+  border-color: rgb(98, 189, 207);
+}
+
+</style>
