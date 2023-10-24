46cdb638eaf0211278fbdb1917cb984192544f29
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2023-10-24 17:05:08 -0600

    Update MainPage.vue

--- frontend/src/pages/MainPage.vue
+++ frontend/src/pages/MainPage.vue
@@ -1,3 +1,23 @@
 <template>
-    <h1>Main Page</h1>
+    <div class="mainpage">
+        <base-button mode="big-buttons">10 min unpaid break</base-button>
+        <base-button mode="big-buttons">30 min unpaid break</base-button>
+        <base-button mode="big-buttons">Clock In</base-button>
+    </div>
 </template>
+
+<style scoped>
+.mainpage {
+  text-align: center; /* Horizontally center the content */
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* Vertically center the content */
+  height: 30vh; /* Set the height to 100% of the viewport height for vertical centering */
+  justify-content: center; /* Center content both horizontally and vertically */
+  background-color: white;
+  margin-top: 10%;
+  margin-left: 25%;
+  margin-right: 25%;
+}
+
+</style>
