4fb3944512e5236fd354c152c985e98009a8ea2a
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2023-10-24 17:55:51 -0600

    Update router.js

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,13 +1,15 @@
 import  { createRouter, createWebHistory } from 'vue-router';
 import LogIn from './pages/LogIn.vue';
-import MainPage from './pages/MainPage.vue';
+import MainPage from './pages/worker/MainPage.vue';
+import ClockedIn from './pages/worker/ClockedIn.vue';
 
 const router = createRouter({
     history: createWebHistory(),
     routes: [
         {path: '/', redirect: '/login'},
         {path: '/login', component: LogIn},
-        {path: '/main/:id', component: MainPage}
+        {path: '/main/:id', component: MainPage},
+        {path: '/:id/ClockedIn', component: ClockedIn}
     ]
 });
 
