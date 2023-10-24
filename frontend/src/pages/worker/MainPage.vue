5dc5d139177379e158c59d3870c0fb6723f084dd
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2023-10-24 17:55:49 -0600

    Create MainPage.vue

---
+++ frontend/src/pages/worker/MainPage.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="mainpage">
+        <h2>Welcome {{ user }}!</h2>
+        <base-button mode="big-buttons">10 min unpaid break</base-button>
+        <base-button mode="big-buttons">30 min unpaid break</base-button>
+        <base-button v-if="!isClockedIn"  mode="big-buttons" @click="startShift">Clock In</base-button>
+        <base-button v-else  mode="big-buttons">Clock Out</base-button>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            user: this.$route.params.id,
+            isClockedIn: false
+        };
+    },
+    // created(){
+    //     //talk to backend to check if user is clocked in or not
+    // },
+    methods:{
+        startShift(){
+            this.$router.push('/'+this.user+'/ClockedIn');
+        }
+    }
+}
+</script>
+
+<style scoped>
+.mainpage {
+  text-align: center; /* Horizontally center the content */
+  display: flex;
+  flex-direction: column;
+  align-items: center; /* Vertically center the content */
+  height: 30vh; /* Set the height to 100% of the viewport height for vertical centering */
+  justify-content: center; /* Center content both horizontally and vertically */
+  background-color: white;
+  margin-top: 15%;
+  margin-left: 25%;
+  margin-right: 25%;
+}
+
+</style>
