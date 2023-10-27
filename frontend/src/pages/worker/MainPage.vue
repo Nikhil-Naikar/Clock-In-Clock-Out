aa96992f5febb835facd30133d003bb8a0d60902
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2023-10-27 15:16:27 -0600

    Update MainPage.vue

--- frontend/src/pages/worker/MainPage.vue
+++ frontend/src/pages/worker/MainPage.vue
@@ -1,10 +1,17 @@
 <template>
     <div class="mainpage">
         <h2>Welcome {{ user }}!</h2>
-        <base-button mode="big-buttons">10 min unpaid break</base-button>
-        <base-button mode="big-buttons">30 min unpaid break</base-button>
-        <base-button v-if="!isClockedIn"  mode="big-buttons" @click="startShift">Clock In</base-button>
-        <base-button v-else  mode="big-buttons" @click="endShift">Clock Out</base-button>
+        <div class="grid-two-col">
+            <div>
+                <img src="../../assets/clock.png">
+            </div>
+            <div class="selection-container">
+                <base-button mode="big-buttons">10 min unpaid break</base-button>
+                <base-button mode="big-buttons">30 min unpaid break</base-button>
+                <base-button v-if="!isClockedIn"  mode="big-buttons" @click="startShift">Clock In</base-button>
+                <base-button v-else  mode="big-buttons" @click="endShift">Clock Out</base-button>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -32,16 +39,39 @@ export default {
 
 <style scoped>
 .mainpage {
-  text-align: center; /* Horizontally center the content */
-  display: flex;
-  flex-direction: column;
-  align-items: center; /* Vertically center the content */
-  height: 30vh; /* Set the height to 100% of the viewport height for vertical centering */
+  height: 50vh; /* Set the height to 100% of the viewport height for vertical centering */
   justify-content: center; /* Center content both horizontally and vertically */
   background-color: white;
-  margin-top: 15%;
-  margin-left: 25%;
-  margin-right: 25%;
+  margin-left: 20%;
+  margin-right: 20%;
+  border-radius: 10%;
+  border: 5px solid black;
 }
+
+.grid-two-col {
+    display: grid;
+    grid-template-columns: 60% 40%;
+    padding: 50px;
+}
+
+img {
+    max-width: 100%;
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+}
+
+.selection-container {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    align-items: center;
+    justify-content: center;
+}
+
+
 
 </style>
