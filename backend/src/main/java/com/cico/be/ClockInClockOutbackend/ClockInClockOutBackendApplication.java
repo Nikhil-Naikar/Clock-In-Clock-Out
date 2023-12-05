9bafb13fab5d7409f9c6a282a884f1eeeb2cff6f
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2023-12-05 15:48:14 -0700

    Create ClockInClockOutBackendApplication.java

---
+++ backend/src/main/java/com/cico/be/ClockInClockOutbackend/ClockInClockOutBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cico.be.ClockInClockOutbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClockInClockOutBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClockInClockOutBackendApplication.class, args);
+	}
+
+}
