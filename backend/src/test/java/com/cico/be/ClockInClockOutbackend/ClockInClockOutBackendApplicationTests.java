03d793bb1169a4e8aab18649cb4549399690b870
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2023-12-05 15:48:19 -0700

    Create ClockInClockOutBackendApplicationTests.java

---
+++ backend/src/test/java/com/cico/be/ClockInClockOutbackend/ClockInClockOutBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cico.be.ClockInClockOutbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClockInClockOutBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
