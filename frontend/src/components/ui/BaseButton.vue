27c07ce52c23f324acce62820367e3e592a51553
WonderBoi99 <61436662+WonderBoi99@users.noreply.github.com>
2024-01-07 15:33:21 -0700

    Update BaseButton.vue

--- frontend/src/components/ui/BaseButton.vue
+++ frontend/src/components/ui/BaseButton.vue
@@ -15,7 +15,7 @@ export default {
 button{
   text-decoration: none;
   padding: 0.5rem 1rem;
-  font-size: 1.25rem;
+  font-size: 1.2rem;
   background-color: rgb(3, 107, 128);
   border: 1px solid rgb(98, 189, 207);
   color: white;
